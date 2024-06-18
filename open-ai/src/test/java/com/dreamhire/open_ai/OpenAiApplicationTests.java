db19c95c34b80cbd21d52ab9d16b8d3a6a0591ce
yashV0201 <vardhan.yash0201@gmail.com>
2024-06-18 16:19:33 +0530

    openAi services added

---
+++ open-ai/src/test/java/com/dreamhire/open_ai/OpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamhire.open_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
